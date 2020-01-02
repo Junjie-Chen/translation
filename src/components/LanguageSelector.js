44630eec626a5039332daea74cec68006418d668
Junjie Chen <junjie.chen18@gmail.com>
2020-01-02 08:52:02 -0500

    feat(component): render American and Dutch national flag icons in language selector

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class LanguageSelector extends Component {
   render() {
-    return <div>Language Selector</div>;
+    return (
+      <div>
+        Select a Language:&nbsp;&nbsp;
+        <i className="flag us"></i>
+        <i className="flag nl"></i>
+      </div>
+    );
   }
 }
 
