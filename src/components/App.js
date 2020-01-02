c6f359049f70320db2529d076439a79ad4001917
Junjie Chen <junjie.chen18@gmail.com>
2020-01-02 09:01:48 -0500

    feat(component): render form in browser

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import LanguageSelector from './LanguageSelector';
+import Form from './Form';
 
 class App extends Component {
   render() {
     return (
       <div className="ui container">
         <LanguageSelector />
+        <Form />
       </div>
     );
   }
