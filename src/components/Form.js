9f8d68f2a524cbe1db9eac9767cf06d583f790fd
Junjie Chen <junjie.chen18@gmail.com>
2020-01-02 08:57:44 -0500

    feat(component): render form field in browser

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Field from './Field';
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <div className="ui form">
+      <Field />
+    </div>
+  );
 };
 
 export default Form;
