6d144e6c3e063aa225aef37e1e91af121ad801ed
Junjie Chen <junjie.chen18@gmail.com>
2020-01-02 09:00:16 -0500

    feat(component): render button text conditionally

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
+  renderSubmit(language) {
+    return language === 'English' ? 'Submit' : 'Voorleggen';
+  }
+
   renderButton() {
-    return <button className="ui button primary">Submit</button>;
+    return <button className="ui button primary">{this.renderSubmit('English')}</button>;
   }
 
   render() {
