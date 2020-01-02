a9bdb28dbc5d27587a7d6dbcc57f489566041e0c
Junjie Chen <junjie.chen18@gmail.com>
2020-01-02 09:14:45 -0500

    feat(context): link Field component to LanguageContext object

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends Component {
+  static contextType = LanguageContext;
+
   render() {
     return (
       <div className="ui field">
