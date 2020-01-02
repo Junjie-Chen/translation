db27830dd5f285f51f48353467848a4ac3e6cbe3
Junjie Chen <junjie.chen18@gmail.com>
2020-01-02 09:17:07 -0500

    feat(context): link button to LanguageContext object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends Component {
   renderSubmit(language) {
@@ -6,7 +7,13 @@ class Button extends Component {
   }
 
   renderButton() {
-    return <button className="ui button primary">{this.renderSubmit('English')}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {({ language }) => this.renderSubmit(language)}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 
   render() {
