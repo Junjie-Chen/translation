6761f5b1c16af459140c918f553ae22fa2bd3610
Junjie Chen <junjie.chen18@gmail.com>
2020-01-02 09:07:26 -0500

    feat(context): pass value to LanguageContext object

--- src/components/LanguageStore.js
+++ src/components/LanguageStore.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class LanguageStore extends Component {
+  state = { language: 'English' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
     return (
-      <LanguageContext.Provider>
+      <LanguageContext.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
         {this.props.children}
       </LanguageContext.Provider>
     );
