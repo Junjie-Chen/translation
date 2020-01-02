a72eb4b49b272534b12f5be817d991ea40eba0b3
Junjie Chen <junjie.chen18@gmail.com>
2020-01-02 08:50:35 -0500

    feat(component): create LanguageSelector component

---
+++ src/components/LanguageSelector.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class LanguageSelector extends Component {
+  render() {
+    return <div>Language Selector</div>;
+  }
+}
+
+export default LanguageSelector;
