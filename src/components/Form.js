576ffde642842900fb6da765e07f7779d8927e1b
Junjie Chen <junjie.chen18@gmail.com>
2020-01-02 09:01:08 -0500

    feat(component): render form button in browser

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Field from './Field';
+import Button from './Button';
 
 const Form = () => {
   return (
     <div className="ui form">
       <Field />
+      <Button />
     </div>
   );
 };
