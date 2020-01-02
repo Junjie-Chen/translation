794085307f6285e8359f62fb921aa138e1c57e6a
Junjie Chen <junjie.chen18@gmail.com>
2020-01-02 09:13:55 -0500

    feat(context): add onClick event and its handler to Dutch national flag icon

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -9,7 +9,7 @@ class LanguageSelector extends Component {
       <div>
         Select a Language:&nbsp;&nbsp;
         <i className="flag us" onClick={() => this.context.onLanguageChange('English')}></i>
-        <i className="flag nl"></i>
+        <i className="flag nl" onClick={() => this.context.onLanguageChange('Dutch')}></i>
       </div>
     );
   }
